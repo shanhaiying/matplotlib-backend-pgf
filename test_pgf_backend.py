402aba1f4c0eb27e2a749dd25a71f0c1a00d7f15
Peter Würtz <pwuertz@googlemail.com>
2012-06-28 14:04:51 +0200

    update the demo

--- test_pgf_backend.py
+++ test_pgf_backend.py
@@ -2,7 +2,7 @@
 
 import matplotlib
 
-use_pgf = True
+use_pgf = False
 if use_pgf:
     matplotlib.use('module://backend_pgf')
     matplotlib.rcParams.update({"pgf.font": "CMU Serif"})
@@ -12,7 +12,7 @@ else:
     \usepackage{lmodern}
     \usepackage[T1]{fontenc}
     """
-    matplotlib.rcParams["text.usetex"] = False
+    matplotlib.rcParams["text.usetex"] = True
     matplotlib.rcParams['text.latex.unicode'] = True
     matplotlib.rcParams["font.family"] = "serif"
 
@@ -32,12 +32,23 @@ x = np.linspace(0, 1, num=20)
 
 p.figure(figsize=(2.6,2.0))
 p.plot(x, x**2, "r--", label=ur"Unicode, ияäüέψλ", lw=2)
-p.plot(x, 1-x**2, "b-.", label=ur"Math, $\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")
+if matplotlib.rcParams["text.usetex"]:
+    p.plot(x, 1-x**2, "b-.", label=ur"Math, $\displaystyle\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")
+else:
+    p.plot(x, 1-x**2, "b-.", label=ur"Math, $\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")    
 p.plot(x, 0.2*x, "g>")
 p.xlabel(ur"$x$-axis in units of $10^3\,$µm")
 p.ylabel(ur"angle $\alpha$ in °")
 p.tight_layout(0.0)
 p.legend()
 
-if use_pgf: p.savefig("figure.pgf")
-p.savefig("figure.pdf")
+if use_pgf:
+    print "saving pgf figures in demo/"
+    p.savefig("demo/figure-pgf.pdf")
+    p.savefig("demo/figure.pgf")
+elif matplotlib.rcParams["text.usetex"]:
+    print "saving pdf-usetex figure in demo/"
+    p.savefig("demo/figure-pdf-usetex.pdf")
+else:
+    print "saving pdf figure in demo/"
+    p.savefig("demo/figure-pdf.pdf")
