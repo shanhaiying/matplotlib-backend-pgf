160d087bfd3aa6162ffe2e591e85498c09e2bbab
Peter Würtz <pwuertz@googlemail.com>
2012-07-02 22:31:42 +0200

    updated demo

--- demo/create_demo_figures.py
+++ demo/create_demo_figures.py
@@ -1,60 +1,72 @@
 # -*- coding: utf-8 -*-
+# create a figure for different rc parameter sets
+rc_sets = []; fnames_sets = []
 
-import matplotlib
+# pgf backend
+rc_sets.append({
+    "backend": "module://backend_pgf",
+    "font.serif": ["CMU Serif"], "font.sans-serif": ["CMU Sans Serif"],
+    "font.monospace": ["CMU Concrete"],
+    })
+fnames_sets.append(["figure-pgf.pdf", "figure.pgf"])
 
-use_pgf = True
+# pdf backend with text.usetex
+rc_sets.append({
+    "text.usetex": True, "text.latex.unicode": True,
+    "text.latex.preamble": ["\\usepackage{lmodern}", "\\usepackage[T1]{fontenc}"],
+    })
+fnames_sets.append(["figure-pdf-usetex.pdf"])
 
-if use_pgf:
-    matplotlib.use('module://backend_pgf')
-    # set fonts using matplotlib rc
-    matplotlib.rcParams.update({"pgf.rcfonts": True})
-    matplotlib.rcParams["font.family"] = "serif"
-    matplotlib.rcParams["font.serif"] = ["CMU Serif"]
-    matplotlib.rcParams["font.sans-serif"] = ["CMU Sans Serif"]
-    matplotlib.rcParams["font.monospace"] = ["CMU Concrete"]
-    #matplotlib.rcParams.update({"pgf.preamble": r"\usepackage{mathpazo}"})
-else:
-    matplotlib.rcParams["text.latex.preamble"] = r"""
-    \usepackage{lmodern}
-    \usepackage[T1]{fontenc}
-    """
-    matplotlib.rcParams["text.usetex"] = True
-    matplotlib.rcParams['text.latex.unicode'] = True
-    matplotlib.rcParams["font.family"] = "serif"
+# plain pdf backend
+rc_sets.append({
+    "text.usetex": False,
+    })
+fnames_sets.append(["figure-pdf.pdf"])
 
-params_text = {
-               'axes.labelsize': 11,
-               'text.fontsize': 11,
-               'legend.fontsize': 11,
-               'xtick.labelsize': 11,
-               'ytick.labelsize': 11,
-               }
-matplotlib.rcParams.update(params_text)
+##############################################################################
 
-import pylab as p
+# fork child process for each set of rc parameters
+import os
+parent_pid = os.getpid()
+child_pids = []
+for rc_set, fnames in zip(rc_sets, fnames_sets):
+    if os.getpid() == parent_pid:
+        pid = os.fork()
+        if pid == 0:
+            break
+        child_pids.append(pid)
+# wait for all children to finish
+if os.getpid() == parent_pid:
+    for pid in child_pids: os.waitpid(pid, 0)
+    exit()
+
+##############################################################################
+
+# actual plotting script starts here
+
+import matplotlib as mpl
+rc_text = {"font.family": "serif", "axes.labelsize": 11, "text.fontsize": 11,
+           "legend.fontsize": 11, "xtick.labelsize": 11, "ytick.labelsize": 11}
+mpl.rcParams.update(rc_text)
+mpl.rcParams.update(rc_set)
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1, num=20)
 
-p.figure(figsize=(2.6,2.0))
-p.plot(x, x**2, "r--", label=ur"Unicode, ияäüέψλ", lw=2)
-if matplotlib.rcParams["text.usetex"]:
-    p.plot(x, 1-x**2, "b-.", label=ur"Math, $\displaystyle\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")
+plt.figure(figsize=(2.6,2.0))
+plt.plot(x, x**2, "r--", label=ur"Unicode, ияäüέψλ", lw=2)
+if mpl.rcParams["text.usetex"]:
+    plt.plot(x, 1-x**2, "b-.", label=ur"Math, $\displaystyle\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")
 else:
-    p.plot(x, 1-x**2, "b-.", label=ur"Math, $\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")    
-p.plot(x, 0.2*x, "g>")
-p.xlabel(ur"$x$-axis in units of $10^3\,$µm")
-p.ylabel(ur"angle $\alpha$ in °")
-p.tight_layout(0.0)
-p.legend()
+    plt.plot(x, 1-x**2, "b-.", label=ur"Math, $\int_\Omega \mu \cdot x^2\,\mathrm{d}x$")    
+plt.plot(x, 0.2*x, "g>")
+plt.xlabel(ur"$x$-axis in units of $10^3\,$µm")
+plt.ylabel(ur"angle $\alpha$ in °")
+plt.tight_layout(0.0)
+plt.legend()
 
-if use_pgf:
-    print "saving pgf figures"
-    p.savefig("figure.pgf")
-    p.savefig("figure-pgf.pdf")    
-elif matplotlib.rcParams["text.usetex"]:
-    print "saving pdf-usetex figure"
-    p.savefig("figure-pdf-usetex.pdf")
-else:
-    print "saving pdf figure"
-    p.savefig("figure-pdf.pdf")
+# save figure
+for fname in fnames:
+    plt.savefig(fname)
